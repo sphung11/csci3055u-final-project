3c87512b5da8f5e91169773a498cdeea1ca36f2f
Steph <stephanie.phung1@uoit.net>
2018-12-11 21:34:51 -0500

    Add files via upload

---
+++ basic-syntax/sample_code.erl
@@ -0,0 +1,26 @@
+% Erlang does not allow an easy way to demonstrate 
+% the basic syntax of the language without creating 
+% an entire program, so this is only a quick simulation
+% of the output of basic arithmetics into the Eshell.
+
+% To run the program first compile then use
+% sample_code:main().
+% from the Eshell.
+
+-module(sample_code).
+-export([main/0]).
+
+main() ->
+   print(_ = 'Simulating Eshell Output:'),
+   print(2 + 15),
+   print(5 / 2),
+   print(5 div 2),
+   print(5 rem 2),
+   print((2 + 15) - 5 / 2),
+   print(2#101010),
+   print(2.3e3),
+   print(2.3e-3).
+
+print(A) ->
+    io:format("~p", [A]),
+    io:nl().
